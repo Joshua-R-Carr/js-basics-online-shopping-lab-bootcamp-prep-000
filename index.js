7972935e99b080eeee5c15e818e81653e28cb525
josh.ray.carr <josh.ray.carr@gmail.com>
2019-04-30 03:52:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,28 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ item = {
+   itemName : String(item),
+   itemPrice: Math.floor(Math.random() * 101)
+ };
+ cart.push(item);
+ return item.itemName + ' has been added to your cart.';
 }
 
 function viewCart() {
-  // write your code here
+    var answer = 'In your cart, you have ';
+if (cart.length !== 0){
+  for(var i = 0; i >= cart.length; i++){
+    answer  += cart[i].itemName + ' at ' + cart[i].itemPrice;
+    if(i !== cart.length-1){
+      answer += ',';
+    } else {
+      answer += '.';
+    }
+  } return answer;
+} else {
+  return 'Your shopping cart is empty.';
+}
 }
 
 function total() {
